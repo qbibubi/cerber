54a038a82583922d4dd7484b315776f4ba6c12d4
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2023-12-15 07:33:09 +0100

    Change to IPV6

--- main.c
+++ main.c
@@ -11,12 +11,12 @@ int main(int argc, char *argv[])
     AF_INET - Internet domain sockets for use with IPV4 addresses.
     SOCK_STREAM - Byte-stream socket.
   */
-  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  int server_socket = socket(AF_INET6, SOCK_STREAM, 0);
 
   // create server
   const int PORT = 2727;
   struct sockaddr_in server_address;
-  server_address.sin_family = AF_INET;
+  server_address.sin_family = AF_INET6;
   server_address.sin_port = htons(PORT);
   server_address.sin_addr.s_addr = INADDR_ANY;
 
